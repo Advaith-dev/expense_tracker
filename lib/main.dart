75ba35d1a3e7355978b11fa965ff326d702bc0b2
Advaith Manoj <advaithmanoj10@gmail.com>
2025-12-22 23:39:08 +0530

    add dark mode theme and chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 97, 255, 18),
 );
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 0, 28, 23),
+  brightness: Brightness.dark,
+);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -32,6 +45,7 @@ void main() {
         ),
       ),
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
